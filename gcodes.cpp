374ea46ba2972a9f5b95c0dfe3618cc576fe84c4
Aino Nummikallio <aino.nummikallio@gmail.com>
2017-10-01 08:44:25 +0300

    gcodes

---
+++ gcodes.cpp
@@ -0,0 +1,16 @@
+/*
+ * gcodes.cpp
+ *
+ *  Created on: Sep 21, 2017
+ *      Author: aino
+ */
+#include "gcodes.h"
+class gCodes {
+
+
+	gCodes::commands gCodes::get() {
+
+		return gCodes::commands;
+	}
+};
+
